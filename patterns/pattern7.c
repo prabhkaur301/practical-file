579b1b64d93b3a52465eb7e054a737f38979585a
prabhkaur301 <57444962+prabhkaur301@users.noreply.github.com>
2019-12-14 21:38:33 +0530

    Update Pattern7.c

--- patterns/pattern7.c
+++ patterns/pattern7.c
@@ -1,15 +1,17 @@
+
+
+
+
+
 #include<stdio.h>
-int main()
-{
-for(int i=1;i<=7;i++)
-{
-for(int j=1;j<=6;j++)
-
-
-
-
-
-
-
-return 0;
+int main() {
+    int i,j,rows;
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    for (i=1; i<=rows; ++i) {
+        for (j=1; j<=i; ++j)
+        { printf("%d ",j); }
+        printf("\n");
+    }
+    return 0;
 }
